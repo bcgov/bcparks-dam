4c2ca209d205f438a14e412c6d7a0ee1ab34c55c
Fiddler <Robert.Fiddler@gov.bc.ca>
2024-04-02 14:01:02 -0700

    Update outputs.tf

--- src/terraform/outputs.tf
+++ src/terraform/outputs.tf
@@ -1,9 +1,9 @@
-output "url" {
-  description = "Base URL for Resourcspace."
-  value       = "https://${var.service_names[0]}.[LICENCEPLATE]-${var.target_env}.nimbus.cloud.gov.bc.ca/"
-}
-
 output "db" {
   description = "Aurora database endpoint"
   value       = aws_rds_cluster.mysql.endpoint
+}
+
+output "url" {
+  description = "Base URL for Resourcspace"
+  value       = "https://${var.service_names[0]}.[LICENCEPLATE]-${var.target_env}.nimbus.cloud.gov.bc.ca/"
 }
