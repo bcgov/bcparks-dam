060eb9c54b1656dcded44b2a1a9a721c33ac7085
Fiddler <Robert.Fiddler@gov.bc.ca>
2024-04-03 16:22:22 -0700

    Update main.tf

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -59,7 +59,7 @@ resource "aws_cloudfront_distribution" "alb_distribution" {
   viewer_certificate {
     acm_certificate_arn = local.secrets.certificate_arn
     ssl_support_method  = "sni-only"
-    minimum_protocol_version = "TLSv1.2_2019"
+    minimum_protocol_version = "TLSv1.2_2021"
     cloudfront_default_certificate = false
   }
 }
