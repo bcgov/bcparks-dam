cf0d4b4ad281ac377d26525145fecdf99455994c
Fiddler <Robert.Fiddler@gov.bc.ca>
2024-04-03 15:56:20 -0700

    Set API gateway default route throttling

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -93,6 +93,11 @@ resource "aws_apigatewayv2_stage" "app" {
   api_id      = aws_apigatewayv2_api.app.id
   name        = "$default"
   auto_deploy = true
+
+  default_route_settings {
+    throttling_burst_limit = 5000
+    throttling_rate_limit  = 10000
+  }
 }
 
 
