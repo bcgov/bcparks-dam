2208095356e32acc191c3ef725ea9fb2cf54da69
Fiddler <Robert.Fiddler@gov.bc.ca>
2024-12-06 14:45:39 -0800

    Update update_slideshow.sh

--- src/resourcespace/files/update_slideshow.sh
+++ src/resourcespace/files/update_slideshow.sh
@@ -2,7 +2,7 @@
 
 # Find all slideshow directories, print modification time and path
 # Then sort them by modification time, oldest first
-readarray -t directories < <(find /opt/bitnami/resourcespace/filestore/system/ -type d -name "slideshow_*" -printf '%T+ %p\n' | sort | cut -d' ' -f2-)
+readarray -t directories < <(find /var/www/resourcespace/filestore/system/ -type d -name "slideshow_*" -printf '%T+ %p\n' | sort | cut -d' ' -f2-)
 
 # The first element in the sorted array is the oldest directory
 keep_directory="${directories[0]}"
@@ -10,7 +10,7 @@ echo "Keeping oldest slideshow dir: $keep_directory"
 directory_name=$(basename "$keep_directory")
 
 # Update the config.php with the oldest directory
-sudo sed -i "s|'filestore/system/slideshow_[^']*'|'filestore/system/$directory_name'|" /opt/bitnami/resourcespace/include/config.php
+sudo sed -i "s|'filestore/system/slideshow_[^']*'|'filestore/system/$directory_name'|" /var/www/resourcespace/include/config.php
 echo "Slideshow directory in config.php updated to filestore/system/$directory_name"
 
 # Remove all other slideshow directories
