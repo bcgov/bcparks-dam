2d4faaaee81ae38586bec8480590731d46ee78ce
Fiddler <Robert.Fiddler@gov.bc.ca>
2024-12-05 16:37:16 -0800

    Update variables.tf

--- src/terraform/variables.tf
+++ src/terraform/variables.tf
@@ -81,6 +81,7 @@ variable "alb_name" {
 
 variable "domain_name" {
   description = "The domain name for the application"
+  default     = "dam.lqc63d-dev.nimbus.cloud.gov.bc.ca"
   type        = string
 }
 
