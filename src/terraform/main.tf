03183bde19165c5c1c24621f165c8083221cf3a3
Fiddler <Robert.Fiddler@gov.bc.ca>
2025-01-03 11:30:48 -0800

    Set a ForceUpdate tag to force updates to the EC2 image

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -88,7 +88,13 @@ module "asg" {
   version = "5.0.0"
 
   name = "bcparks-dam-vm"
-  tags = var.common_tags
+  #tags = var.common_tags
+  tags = merge(
+    var.common_tags,
+    {
+      "ForceUpdate" = "20250103-1130" # Increment this value to force instance updates
+    }
+  )
 
   # Launch configuration creation
   lc_name                   = var.lc_name
