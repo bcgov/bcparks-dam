41b2aadddeb3fad21760a7a12b327cd5577aaaae
Rob Fiddler <36241237+rfiddler@users.noreply.github.com>
2026-01-29 15:23:12 -0800

    Update outputs.tf

--- src/terraform/outputs.tf
+++ src/terraform/outputs.tf
@@ -3,6 +3,11 @@ output "db" {
   value       = aws_rds_cluster.mysql.endpoint
 }
 
+output "alb_dns_name" {
+  description = "DNS name of the Application Load Balancer"
+  value       = aws_lb.main.dns_name
+}
+
 output "url" {
   description = "Base URL for Resourcespace"
   value       = "https://${var.service_names[0]}.${var.licence_plate}-${var.target_env}.internal.stratus.cloud.gov.bc.ca/"
