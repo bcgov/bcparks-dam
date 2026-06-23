1db20f4311e11d1fdfadaa797d728a9a22d5e64f
rfiddler <rfiddler@yahoo.com>
2026-06-23 14:04:22 -0700

    Update to release 10.7-r29185

--- src/terraform/variables.tf
+++ src/terraform/variables.tf
@@ -36,7 +36,7 @@ variable "branch_name" {
 variable "resourcespace_release_version" {
   description = "ResourceSpace release directory to deploy"
   type        = string
-  default     = "10.7-r28840"
+  default     = "10.7-r29185"
 }
 
 variable "git_url" {
