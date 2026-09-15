342c9985e51be3a74b4a74d9cef481c2c72a4f65
rfiddler <rfiddler@yahoo.com>
2026-09-15 11:32:08 -0700

    Update to 11.0-r29718

--- src/terraform/variables.tf
+++ src/terraform/variables.tf
@@ -36,7 +36,7 @@ variable "branch_name" {
 variable "resourcespace_release_version" {
   description = "ResourceSpace release directory to deploy"
   type        = string
-  default     = "11.0-r29609"
+  default     = "11.0-r29718"
 }
 
 variable "git_url" {
